1c4059bd06d5633f821f6abd1a81dee82f3aeadb
Tran Duc Long <88131960+tlogblue@users.noreply.github.com>
2022-08-12 12:29:50 +0700

    Update play.py

--- play.py
+++ play.py
@@ -5,33 +5,26 @@ from numpy import iterable
 class Solution:
 
     def fizzBuzz(self, n: int) -> list[str]: 
-        # ans list
         ans = []
 
         for num in range(1,n+1):
-
-            divisible_by_3 = (num % 3 == 0)
-            divisible_by_5 = (num % 5 == 0)
-
             num_ans_str = ""
 
-            if divisible_by_3:
-                # Divides by 3
+            if num % 3 == 0:
                 num_ans_str += "Fizz"
-            if divisible_by_5:
-                # Divides by 5
+            if num % 5 == 0:
                 num_ans_str += "Buzz"
             if not num_ans_str:
-                # Not divisible by 3 or 5
                 num_ans_str = str(num)
 
-            # Append the current answer str to the ans list
             ans.append(num_ans_str)  
 
         return ans
     #------------------------------------------------------
-        for fizzbuzz in range(51):
+        n = int(input("nhap so: ") 
+        for fizzbuzz in range(n):
         if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        #if fizzbuzz % 15 == 0: 
             print("fizzbuzz")
             continue
         elif fizzbuzz % 3 == 0:
